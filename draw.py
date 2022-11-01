a16cf92c986444eadbc4e9aaba04f9071138e3d4
ZhangJingYu <work@zjyu.net>
2022-11-01 17:17:13 +0800

    初步完成

--- draw.py
+++ draw.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import os
-import watchdog
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+
 
 # Path to the directory to be watched, gaussian software output file
 # 高斯软件输出文件，监测变动
@@ -15,7 +13,7 @@ IMAGE_FILE_NAME = 'ywq.png'
 
 # The number of lines to begin
 # 开始的行数
-BEGIN_LINE = 1
+BEGIN_LINE = 0
 
 
 def check_file(filename):
@@ -35,14 +33,12 @@ def check_file(filename):
                 if 'Step Temp Press TotEng Density' in lines[i]:
                     part_list.append(i)
     part_count = len(part_list)
-    start_line_num = part_list[-1] + 1
+    if BEGIN_LINE:
+        start_line_num = BEGIN_LINE
+    else:
+        start_line_num = part_list[-1] + 1
     print('预检测完成，共有{}段数据, 分别位于第 {} 行，（！！！注意：从0开始计数！！！）'.format(part_count, part_list))
     print('默认从第 {} 行开始画图, 当然，也可以自定义开始的行数'.format(start_line_num))
-    # print('如果要自定义开始的行数，请输入行数后回车，否则直接回车')
-    # try:
-    #     start_line_num_input = int(input())
-    # except ValueError:
-    #     raise ValueError('输入的行数不是整数')
 
     return start_line_num
 
@@ -61,13 +57,15 @@ def draw():
         y_list = []
         for line in lines[start_line_num:]:
             line = line.strip()
-            print(line)
             if line == '':
                 continue
-            x = int(line.split()[0])
-            y = float(line.split()[3])
-            x_list.append(x)
-            y_list.append(y)
+            try:
+                x = int(line.split()[0])
+                y = float(line.split()[3])
+                x_list.append(x)
+                y_list.append(y)
+            except Exception as e:
+                continue
     print('数据读取完成，正在画图，请稍等...')
     # 画布大小
     plt.figure(figsize=(100, 60), dpi=40)
@@ -80,27 +78,10 @@ def draw():
     plt.title('YWQ', fontsize=88)
     plt.savefig(IMAGE_FILE_NAME)
     print('画图完成，图片文件名为：{}'.format(IMAGE_FILE_NAME))
-    # plt.show()
+    plt.show()
     plt.close()
-
-    # 最近的一百条
-    plt.figure(figsize=(100, 60), dpi=80)
-    ax = plt.gca()
-    plt.grid()
-    ax.xaxis.get_offset_text().set_fontsize(50)
-    plt.tick_params(labelsize=60, labelcolor='green')
-    plt.plot(x_list[-400::2], y_list[-400::2], linewidth=5)
-    plt.xlabel('Step', fontsize=55, fontweight='bold', color='red')
-    plt.ylabel('TotEng', fontsize=55, fontweight='bold', color='red')
-    plt.title('YWQ_last200', fontsize=88)
-    plt.savefig('ywq_last200.png')
     return True
 
-
-class MyHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print("File changed!")
-        draw()
 
 if __name__ == '__main__':
     draw()
