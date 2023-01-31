bf5167ddfac46904b7a7ca5f42ed59b41285dde0
ZhangJingYu <work@zjyu.net>
2023-02-01 01:27:53 +0800

    移动文件

--- extra7z.py
+++ extra7z.py
@@ -1,8 +1,12 @@
+import time
+
 import py7zr
 import os
+import shutil
 
 
-ROOT_DIR = 'D:\\wbtest'
+ROOT_DIR = r'D:\BaiduNetdiskDownload\wb'
+TARGET_DIR = r'E:\wb'
 
 
 def get_7z_file_path(root_dir):
@@ -12,6 +16,7 @@ def get_7z_file_path(root_dir):
         for file in files:
             if file.endswith('.7z'):
                 file_list.append(os.path.join(root, file))
+                print('find -', file)
     return file_list
 
 
@@ -26,6 +31,44 @@ def extract_7z_file(file_list):
         print(f'删除完成 {file}')
 
 
+# 查找所有包含 "BoBo" 的文件夹
+def find_bobo_dir(root_dir):
+    # 遍历根目录下所有文件夹和子文件夹，返回所有包含 "BoBo" 的文件夹
+    dir_list = []
+    for root, dirs, files in os.walk(root_dir):
+        for dir in dirs:
+            if 'BoBo' in dir:
+                dir_list.append(os.path.join(root, dir))
+                print('find -', dir)
+    return dir_list
+
+
+# 将包含 "BoBo" 的文件夹移动到目标目录, 并统计花费时间
+def move_bobo_dir(dir_list, target_dir):
+    for dir in dir_list:
+        time_start = time.time()
+        try:
+            shutil.move(dir, target_dir)
+            print(f'移动完成 {dir}')
+            time_end = time.time()
+            print(f'花费时间 {time_end - time_start}')
+        except PermissionError:
+            print('有文件移动失败， 权限问题', str(dir))
+            time_end = time.time()
+            print(f'花费时间 {time_end - time_start}')
+            continue
+
+
+# 将 Target 目录下所有文件夹重命名，删掉 ”[BoBoSocks袜啵啵]“ 字符串
+# def rename_dir(target_dir):
+#     for root, dirs, files in os.walk(target_dir):
+#         for dir in dirs:
+#             new_dir = dir.replace('[BoBoSocks袜啵啵]', '')
+#             os.rename(os.path.join(root, dir), os.path.join(root, new_dir))
+#             print(f'重命名完成 {dir} -> {new_dir}')
+
+
 if __name__ == '__main__':
-    file_list = get_7z_file_path(ROOT_DIR)
-    extract_7z_file(file_list)
+    dir_list = find_bobo_dir(ROOT_DIR)
+    move_bobo_dir(dir_list, TARGET_DIR)
+    # rename_dir(TARGET_DIR)
